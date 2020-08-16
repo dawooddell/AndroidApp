69114418555d5a7ed5f725ceaf1c5873f968d4ab
Wooddell <dawoodde@3c22fb33c306.ant.amazon.com>
2020-08-16 09:45:28 -0400

    FirstCommit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import LoginScreen from './Register';
+
+class App extends Component {
+   render() {
+       return (
+       <View>
+           <LoginScreen />
+       </View>
+       );
+   }
+}
+
+export default App;
